01f80a48674aa830c7e62104b524402d9db5abc4
tsukadayasuhiro <yshr2357@gmail.com>
2016-07-24 21:25:34 +0900

    計算ができるよ

---
+++ app/src/main/java/jp/techacademy/yasuhiro/tsukada/calcapp/Main2Activity.java
@@ -0,0 +1,26 @@
+package jp.techacademy.yasuhiro.tsukada.calcapp;
+
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.TextView;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        double value1 = intent.getDoubleExtra("Result", 0);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(value1));
+
+    }
+}
---
+++ app/src/main/java/jp/techacademy/yasuhiro/tsukada/calcapp/MainActivity.java
@@ -0,0 +1,66 @@
+package jp.techacademy.yasuhiro.tsukada.calcapp;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+
+
+        String string1 = editText1.getText().toString();
+        double stringToValue1 = Double.parseDouble(string1);
+        String string2 = editText2.getText().toString();
+        double stringToValue2 = Double.parseDouble(string2);
+
+        double result = 0.0;
+
+        //if文の中で変数を宣言してしまえばいいのだ！
+        //System.out.printInなんて忘れろ！
+
+
+        if (v.getId() == R.id.button1) {
+            result = stringToValue1 + stringToValue2;
+        } else if (v.getId() == R.id.button2) {
+            result = stringToValue1 - stringToValue2;
+        } else if (v.getId() == R.id.button3) {
+            result = stringToValue1 * stringToValue2;
+        } else if (v.getId() == R.id.button4) {
+            result = stringToValue1 / stringToValue2;
+        }
+        System.out.println(result);
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("Result", result );
+
+
+        startActivity(intent);
+    }
+}
